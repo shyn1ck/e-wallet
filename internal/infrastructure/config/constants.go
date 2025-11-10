0bc688b25fd65d08e10bd8ed046c4d2d361ed1bf
shyn1ck <hy.parvizjon@gmail.com>
2025-11-10 14:05:46 +0500

    [refactor]: Remove unused constants for transaction types, wallet types, and currency definitions

--- internal/infrastructure/config/constants.go
+++ internal/infrastructure/config/constants.go
@@ -5,19 +5,3 @@ const (
 	HMACAlgorithmSHA1   = "sha1"
 	HMACAlgorithmSHA256 = "sha256"
 )
-
-// TransactionTypeDeposit Transaction type constants
-const (
-	TransactionTypeDeposit = "deposit"
-)
-
-// Wallet type constants
-const (
-	WalletTypeIdentified   = "identified"
-	WalletTypeUnidentified = "unidentified"
-)
-
-// CurrencyTJS Currency constants
-const (
-	CurrencyTJS = "TJS"
-)
