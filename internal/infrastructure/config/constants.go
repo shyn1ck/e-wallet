7b5c594378ad867ff294a9911fe6e5341ef51814
shyn1ck <hy.parvizjon@gmail.com>
2025-11-10 13:49:13 +0500

    [feature]: Add foundational constants for HMAC algorithms, transaction types, wallet types, and currency definitions

---
+++ internal/infrastructure/config/constants.go
@@ -0,0 +1,23 @@
+package config
+
+// HMAC algorithm constants
+const (
+	HMACAlgorithmSHA1   = "sha1"
+	HMACAlgorithmSHA256 = "sha256"
+)
+
+// TransactionTypeDeposit Transaction type constants
+const (
+	TransactionTypeDeposit = "deposit"
+)
+
+// Wallet type constants
+const (
+	WalletTypeIdentified   = "identified"
+	WalletTypeUnidentified = "unidentified"
+)
+
+// CurrencyTJS Currency constants
+const (
+	CurrencyTJS = "TJS"
+)
